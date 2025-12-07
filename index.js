8c9e69d8356ae7996c68c16dae6aeb1ba696f5e1
abyan <abyanfarhan95@gmail.com>
2025-12-07 19:32:52 +0700

    test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const API_KEY = '04c35731a5ee918f014970082a0088b1';
+const API_KEY = 'c7c00b48014e8ea976e8939e2721d950';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 const BACKDROP_URL = 'https://image.tmdb.org/t/p/original';
